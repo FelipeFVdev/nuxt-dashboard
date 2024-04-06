0fd094ccd24587046b2eff80a6f183ecfdc5af17
FelipeFVdev <feliep8@gmail.com>
2024-04-05 22:05:55 -0300

    feature/configs-tailwind-icons-etc

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "nuxt-icon",
+    "@vueuse/nuxt",
+    "nuxt-headlessui",
+    "@morev/vue-transitions/nuxt",
+  ],
+  tailwindcss: { exposeConfig: true },
+  headlessui: { prefix: "H" },
+  css: ["~/assets/css/tailwind.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  app: {
+    head: {
+      title: "Analytics",
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/icon.svg" },
+        // Inter Fonts
+        { rel: "stylesheet", href: "https://rsms.me/inter/inter.css" },
+        { rel: "preconnect", href: "https://rsms.me/" },
+      ],
+    },
+  },
+});
